c1c5acb79d75b45ba702b9287e00723538aadf6f
Xila Luna <xilaluna2@gmail.com>
2022-09-02 19:24:01 -0700

    [update] add ping

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 )
-var Router * gin.Engine
-func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello Again!",
-		})
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
 	})
-	r.Run()
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", ping)
+	router.Run()
 }
