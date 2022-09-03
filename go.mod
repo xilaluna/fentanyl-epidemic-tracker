8207c65dc568514a413cc9384971c90dc1070ec9
Xila Luna <xilaluna2@gmail.com>
2022-09-03 00:32:16 -0700

    [add] working redis saving articles

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gocolly/colly v1.2.0
 	github.com/joho/godotenv v1.4.0
 )
@@ -14,6 +15,8 @@ require (
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xmlquery v1.3.12 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -21,20 +24,22 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/onsi/gomega v1.20.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/extensions"
 	"github.com/joho/godotenv"
@@ -20,125 +21,135 @@ type Article struct {
 	Date string `json:"date"`
 }
 
+var ctx = context.Background()
 
-func ping(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
 
-func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
-}
+func main() {
+	// Load .env file
+	godotenv.Load()
+	
+	// Connect to Redis
+	url := os.Getenv("REDIS_URL")
+	opt, err := redis.ParseURL(url)
+	if err != nil {
+		panic(err)
+	}
+	rdb := redis.NewClient(opt)
 
-func scrape(c *gin.Context) {
-	articles := []Article{}
-	numberOfPages := 1
+	// Create a gin router
+	router := gin.Default()
 
-	// instantiate default collector and set random User-Agent
-	collector := colly.NewCollector(
-		colly.AllowedDomains("darknetlive.com"),
-		colly.CacheDir("./.cache"),
-	)
-	extensions.RandomUserAgent(collector)
+	router.StaticFile("/", "./static/index.html")
 
-	// init article collector and set random User-Agent
-	articleCollector := collector.Clone()
-	extensions.RandomUserAgent(articleCollector)
-
-	// find articles links and vist them with articleCollector
-	collector.OnHTML("main > section > article > a", func(content *colly.HTMLElement) {
-		link := content.Request.AbsoluteURL(content.Attr("href"))
-		articleCollector.Visit(link)
-	})
-
-	// find pagination number
-	collector.OnHTML("main > nav > ul > li:last-child > a", func(content *colly.HTMLElement) {
-		if numberOfPages == 1 {
-			re := regexp.MustCompile("[0-9]+")
-			stringNumber, _ := strconv.Atoi(re.FindAllString(content.Attr("href"), -1)[0])
-			numberOfPages = stringNumber
-		} else {
-			return
-		}
-	})
-
-	articleCollector.OnHTML("main > article", func(content *colly.HTMLElement) {
-		
-		// loop through all the paragraphs
-		content.ForEachWithBreak("div > p", func(i int, paragraph *colly.HTMLElement) bool {
-			
-			// check paragraph for the word "fentanyl"
-			if strings.Contains(strings.ToLower(paragraph.Text), "fentanyl") {
-				title := content.ChildText("header > h1")
-				date := content.ChildText("aside > div > time")
-
-				fmt.Println("Found article:", title, date)
-
-				// add article to articles slice
-				newArticle := Article{}
-				newArticle.Title = title
-				newArticle.Date = date
-				
-				articles = append(articles, newArticle)
-
-				// stop the loop
-				return false
-			}
-			return true
+	router.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
 
-	articleCollector.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting", request.URL)
-	})
-	
-	collector.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting", request.URL)
-	})
-
-	// start scraping
-	for i := 1; i <= numberOfPages; i++ {
-		if i == 1 {
-			collector.Visit("https://darknetlive.com/post")
-		} else {
-			collector.Visit("https://darknetlive.com/post/page/" + strconv.Itoa(i))
-			
+	router.GET("/articles", func(c *gin.Context) {
+		// Get the articles from Redis
+		articles, err := rdb.Get(ctx, "articles").Result()
+		if err != nil {
+			panic(err)
 		}
-	}
-
-	// convert articles to json
-	json, err := json.Marshal(articles)
-	if err != nil {
-		log.Println(err)
-	}
-
-	// write json to response
-	writeError := os.WriteFile("./assets/data.json", json, 0644)
-	if writeError != nil {
-		log.Println(writeError)
-	}
-
-
-	c.JSON(200, gin.H{
-		"message": "scraped",
+		c.JSON(200, gin.H{
+			"articles": articles,
+		})
 	})
-}
 
-func main() {
-	godotenv.Load()
-	
-	url := os.Getenv("REDIS_URL")
-	fmt.Println(url)
+	router.GET("/scrape", func(c *gin.Context) {
+		articles := []Article{}
+		numberOfPages := 1
 
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+		// instantiate default collector and set random User-Agent
+		collector := colly.NewCollector(
+			colly.AllowedDomains("darknetlive.com"),
+			colly.CacheDir("./.cache"),
+		)
+		extensions.RandomUserAgent(collector)
 
-	router.GET("/", index)
-	router.GET("/ping", ping)
-	router.GET("/scrape", scrape)
+		// init article collector and set random User-Agent
+		articleCollector := collector.Clone()
+		extensions.RandomUserAgent(articleCollector)
 
-	router.StaticFile("/data.json", "./assets/data.json")
+		// find articles links and vist them with articleCollector
+		collector.OnHTML("main > section > article > a", func(content *colly.HTMLElement) {
+			link := content.Request.AbsoluteURL(content.Attr("href"))
+			articleCollector.Visit(link)
+		})
+
+		// find pagination number
+		collector.OnHTML("main > nav > ul > li:last-child > a", func(content *colly.HTMLElement) {
+			if numberOfPages == 1 {
+				re := regexp.MustCompile("[0-9]+")
+				stringNumber, _ := strconv.Atoi(re.FindAllString(content.Attr("href"), -1)[0])
+				numberOfPages = stringNumber
+			} else {
+				return
+			}
+		})
+
+		articleCollector.OnHTML("main > article", func(content *colly.HTMLElement) {
+			
+			// loop through all the paragraphs
+			content.ForEachWithBreak("div > p", func(i int, paragraph *colly.HTMLElement) bool {
+				
+				// check paragraph for the word "fentanyl"
+				if strings.Contains(strings.ToLower(paragraph.Text), "fentanyl") {
+					title := content.ChildText("header > h1")
+					date := content.ChildText("aside > div > time")
+
+					fmt.Println("Found article:", title, date)
+
+					// add article to articles slice
+					newArticle := Article{}
+					newArticle.Title = title
+					newArticle.Date = date
+					
+					articles = append(articles, newArticle)
+
+					// stop the loop
+					return false
+				}
+				return true
+			})
+		})
+
+		articleCollector.OnRequest(func(request *colly.Request) {
+			fmt.Println("Visiting", request.URL)
+		})
+		
+		collector.OnRequest(func(request *colly.Request) {
+			fmt.Println("Visiting", request.URL)
+		})
+
+		// start scraping
+		for i := 1; i <= numberOfPages; i++ {
+			if i == 1 {
+				collector.Visit("https://darknetlive.com/post")
+			} else {
+				collector.Visit("https://darknetlive.com/post/page/" + strconv.Itoa(i))
+				
+			}
+		}
+
+		// convert articles to json
+		json, err := json.Marshal(articles)
+		if err != nil {
+			log.Println(err)
+		}
+
+		err = rdb.Set(ctx, "articles", json, 0).Err()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+
+		c.JSON(200, gin.H{
+			"message": "scraped",
+		})
+	})
 
 	router.Run()
 }
